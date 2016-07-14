b202849be20cd1bd1c5c3f4b973c2180d26b06ee
Bonta Vlad <bonta.vlad@gmail.com>
2016-07-14 22:41:36 +0300

    Improved solution.

--- area_of_room.nim
+++ area_of_room.nim
@@ -1,8 +1,20 @@
 import
   strutils
 
+type Answer = enum aYes, aNo
 
 const conversion_value = 3.2808
+var units = "feet"
+
+proc ask_yes_no(question: string): Answer =
+  echo(question, " (y/n)")
+  while true:
+    case readLine(stdin)
+    of "y", "Y", "yes", "Yes":
+      return Answer.aYes  # Enums can be qualified
+    of "n", "N", "no", "No":
+      return Answer.aNo
+    else: echo("Please be clear: yes or no")
 
 proc ask_question(question: string): int =
   var anwser = ""
@@ -21,11 +33,28 @@ proc calculate_area(lenght, width: int): int =
 proc convert_to_meters(input: int): float =
   return float(input) / conversion_value
 
-let lenght_feet = ask_question("What is the length of the room in feet?: ")
-let width_feet = ask_question("What is the width of the room in feet?: ")
+proc convert_to_feet(input: int): float =
+  return float(input) * conversion_value
 
-let area_feet = calculate_area(lenght_feet, width_feet)
+case ask_yes_no("Use meters? if no we use feet")
+of aYes:
+  units = "meters"
+of aNo:
+  discard
 
-echo "You enter dimensions of $1 feet by $2 feet." % [$lenght_feet, $width_feet]
-echo "Area in feet $#" % [$area_feet]
-echo "Area in meters $#" % [formatFloat(convert_to_meters(area_feet), precision=4)]
+let lenght = ask_question("What is the length of the room in $#?: " % units)
+let width = ask_question("What is the width of the room in $#?: " % units)
+
+
+let area = calculate_area(lenght, width)
+
+echo "You enter dimensions of $1 $3 by $2 $3." % [$lenght, $width, units]
+echo "Area in $# $#" % [units, $area]
+
+case units
+of "meters":
+  echo "Area in feet $#" % [formatFloat(convert_to_feet(area), precision=4)]
+of "feet":
+  echo "Area in meters $#" % [formatFloat(convert_to_meters(area), precision=4)]
+else:
+  discard
